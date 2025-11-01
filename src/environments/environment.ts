2b755b843a6f921d49ebc6b4a2052f31a2b31e9f
cdtafurc <71tz5hhp6gcxqdyYEE8Q>
2025-11-01 11:05:35 -0500

    fix: actualizar configuración de proxy en docker-compose y ajustar apiUrl en environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080', // Apuntando directamente al backend
+  apiUrl: '/api', // Apuntando directamente al backend
 };
